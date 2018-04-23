f606c319ff12d4fc487b682f71461c666e9a2947
Stephan Kristyn <steve@meshfields.de>
2018-04-23 20:38:30 +0200

    adding cors-2

--- service/pods.js
+++ service/pods.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const Client = require('kubernetes-client').Client;
-const config = require('kubernetes-client').config;
-// const Api = require('kubernetes-client');
+// const Client = require('kubernetes-client').Client;
+// const config = require('kubernetes-client').config;
+const Api = require('kubernetes-client');
 const logger = require('../lib/logger');
 
-// const core = new Api.Core(Api.config.getInCluster());
+const core = new Api.Core(Api.config.getInCluster());
 
 module.exports.pods = async () => {
-	const client = new Client({config: config.fromKubeconfig()});
-	await client.loadSpec();
-	return await client.api.v1.namespaces('default').pods.get()
-	// return await core.namespaces('default').pods.get()
+	// const client = new Client({config: config.fromKubeconfig()});
+	// await client.loadSpec();
+	// return await client.api.v1.namespaces('default').pods.get()
+	return await core.namespaces('default').pods.get()
 		.then(result => {
 			return result;
 		})
