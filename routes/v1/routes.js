204a346aca4fe5fc0d90c79d5739d1b77f3597c5
Stephan Kristyn <steve@meshfields.de>
2018-04-23 20:59:48 +0200

    Linting

--- routes/v1/routes.js
+++ routes/v1/routes.js
@@ -5,30 +5,30 @@ const rateLimit = require('micro-ratelimit');
 const controller = require('../../controller/index');
 
 const hello = rateLimit({window: 5000, limit: 2}, (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	send(res, 200, `Hello, ${req.params.who}`);
 });
 
 const notFound = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	send(res, 404, 'HTTP 404 - Endpoint not found');
 };
 
 const namespaces = async (req, res) => {
 	const namespaces = await controller.namespaces.namespaces();
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	send(res, 200, namespaces);
 };
 
 const pods = async (req, res) => {
 	const pods = await controller.pods.pods();
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	send(res, 200, pods);
 };
 
 module.exports = router(
-    post('/hello/:who', hello),
-    get('/namespaces', namespaces),
-    get('/pods', pods),
-    get('/*', notFound),
+	post('/hello/:who', hello),
+	get('/namespaces', namespaces),
+	get('/pods', pods),
+	get('/*', notFound),
 );
