26f3d1c98d570a282dd1ebfa7050dad1b2cabe31
Stephan Kristyn <steve@meshfields.de>
2018-04-23 21:23:17 +0200

    res.end middleware fix

--- routes/v1/routes.js
+++ routes/v1/routes.js
@@ -1,34 +1,33 @@
-'use strict';
-const {send} = require('micro');
+// 'use strict';
 const {router, get, post} = require('microrouter');
 const rateLimit = require('micro-ratelimit');
 const controller = require('../../controller/index');
 
 const hello = rateLimit({window: 5000, limit: 2}, (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	send(res, 200, `Hello, ${req.params.who}`);
+	res.end(res, 200, `Hello, ${req.params.who}`);
 });
 
 const notFound = (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	send(res, 404, 'HTTP 404 - Endpoint not found');
+	res.end(res, 404, 'HTTP 404 - Endpoint not found');
 };
 
 const namespaces = async (req, res) => {
 	const namespaces = await controller.namespaces.namespaces();
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	send(res, 200, namespaces);
+	res.end(res, 200, namespaces);
 };
 
 const pods = async (req, res) => {
 	const pods = await controller.pods.pods();
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	send(res, 200, pods);
+	res.end(res, 200, pods);
 };
 
 module.exports = router(
 	post('/hello/:who', hello),
 	get('/namespaces', namespaces),
 	get('/pods', pods),
-	get('/*', notFound),
+	get('/*', notFound)
 );
