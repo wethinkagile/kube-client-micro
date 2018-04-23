91e6c73d049adb74d2ef9ff971b32c627fa8851b
Stephan Kristyn <steve@meshfields.de>
2018-04-23 16:22:08 +0200

    Linting

--- service/pods.js
+++ service/pods.js
@@ -1,24 +1,18 @@
 'use strict';
 
-const logger = require('../lib/logger');
-
-// Access via KubeConfig
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
-
-// Access via InCluster API
 const Api = require('kubernetes-client');
+const logger = require('../lib/logger');
+
 const core = new Api.Core(Api.config.getInCluster());
 
 module.exports.pods = async () => {
 	const client = new Client({config: config.fromKubeconfig()});
 	await client.loadSpec();
 
-    // Access via KubeConfig
 	// return await client.api.v1.namespaces('default').pods.get()
-	
-	// Access via InCluster API
-    return await core.namespaces('default').pods.get()
+	return await core.namespaces('default').pods.get()
 		.then(result => {
 			return result;
 		})
