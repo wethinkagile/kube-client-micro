e7a0b86c55bcd9a74f93a35f494bfe36b69db5e3
Stephan Kristyn <steve@meshfields.de>
2018-04-23 16:16:39 +0200

    Adding InCluster Access

--- service/pods.js
+++ service/pods.js
@@ -1,14 +1,24 @@
 'use strict';
 
+const logger = require('../lib/logger');
+
+// Access via KubeConfig
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
-const logger = require('../lib/logger');
+
+// Access via InCluster API
+const Api = require('kubernetes-client');
+const core = new Api.Core(Api.config.getInCluster());
 
 module.exports.pods = async () => {
 	const client = new Client({config: config.fromKubeconfig()});
 	await client.loadSpec();
 
-	return await client.api.v1.namespaces('default').pods.get()
+    // Access via KubeConfig
+	// return await client.api.v1.namespaces('default').pods.get()
+	
+	// Access via InCluster API
+    return await core.namespaces('default').pods.get()
 		.then(result => {
 			return result;
 		})
