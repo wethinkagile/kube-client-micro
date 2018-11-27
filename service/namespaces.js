cc270ecd47017954d9ab33666d24652d73b6bc8a
Marcel Oed <marcel@ventx.de>
2018-11-27 21:44:08 +0100

    Refactoring

--- service/namespaces.js
+++ service/namespaces.js
@@ -1,38 +1,24 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'development') {
-	const Client = require('kubernetes-client').Client;
-	const config = require('kubernetes-client').config;
-	const logger = require('../lib/logger');
-
-	module.exports.namespaces = async () => {
-		const client = new Client({config: config.fromKubeconfig(), version: '1.9'});
-		await client.loadSpec();
-
-		return await client.api.v1.namespaces.get()
-			.then(result => {
-				return result;
-			})
-			.catch(err => {
-				logger(err, this);
-				return err;
-			});
-	};
-}
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+const logger = require('../lib/logger');
 
 if (process.env.NODE_ENV === 'production') {
-	const Api = require('kubernetes-client');
-	const logger = require('../lib/logger');
-	const core = new Api.Core(Api.config.getInCluster());
-
-	module.exports.namespaces = async () => {
-		return await core.namespaces.get()
-			.then(result => {
-				return result;
-			})
-			.catch(err => {
-				logger(err, this);
-				return err;
-			});
-	};
+	var client = new Client({ config: config.getInCluster() });
 }
+else { // For development we use our local kubeconfig
+	var client = new Client({ config: config.fromKubeconfig() });
+}
+
+module.exports.namespaces = async () => {
+	await client.loadSpec();
+	return await client.api.v1.namespaces.get()
+		.then(result => {
+			return result;
+		})
+		.catch(err => {
+			logger(err, this);
+			return err;
+		});
+};
