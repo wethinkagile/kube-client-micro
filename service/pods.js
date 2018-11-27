c3f956d5cd871cf3fbca535aaef22a630eb98cfc
Marcel Oed <marcel@ventx.de>
2018-11-27 21:43:34 +0100

    Refactor & add namespace parameter

--- service/pods.js
+++ service/pods.js
@@ -1,39 +1,24 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'development') {
-	const Client = require('kubernetes-client').Client;
-	const config = require('kubernetes-client').config;
-	const logger = require('../lib/logger');
-
-	module.exports.pods = async() => {
-		const client = new Client({config: config.fromKubeconfig()});
-		await client.loadSpec();
-		return await client.api.v1.namespaces('default').pods.get()
-			.then(result => {
-				return result;
-			})
-			.catch(err => {
-				logger(err, this);
-				console.info(err, this);
-				return err;
-			});
-	};
-}
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+const logger = require('../lib/logger');
 
 if (process.env.NODE_ENV === 'production') {
-	const Api = require('kubernetes-client');
-	const logger = require('../lib/logger');
-	const core = new Api.Core(Api.config.getInCluster());
-
-	module.exports.pods = async() => {
-		return await core.namespaces('default').pods.get()
-			.then(result => {
-				return result;
-			})
-			.catch(err => {
-				logger(err, this);
-				console.info(err, this);
-				return err;
-			});
-	};
+	var client = new Client({ config: config.getInCluster() });
 }
+else { // For development we use our local kubeconfig
+	var client = new Client({ config: config.fromKubeconfig() });
+}
+
+module.exports.pods = async (namespace) => {
+	await client.loadSpec();
+	return await client.api.v1.namespaces(namespace).pods.get()
+		.then(result => {
+			return result;
+		})
+		.catch(err => {
+			logger(err, this);
+			return err;
+		});
+};
